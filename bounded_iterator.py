cc5075ceca057fdf6c2120c1a1a3d4396519ca1a
Javier Ruere <javier@ruere.com.ar>
2018-03-03 16:39:54 -0300

    Initial version.

---
+++ bounded_iterator.py
@@ -0,0 +1,42 @@
+import collections
+from threading import BoundedSemaphore
+from typing import Iterable, Iterator, TypeVar
+
+
+T = TypeVar('T')
+
+
+class BoundedIterator(collections.Iterator):
+    """Limits the number of values to yield until yielded values are
+    acknowledged.
+    """
+
+    def __init__(self, bound, it: Iterable[T]):
+        self._it = iter(it)
+
+        self._sem = BoundedSemaphore(bound)
+
+    def __iter__(self) -> Iterator[T]:
+        return self
+
+    def __next__(self) -> T:
+        return self.next()
+
+    def next(self, timeout=None) -> T:
+        """Returns the next value from the iterable.
+
+        This method is not thread-safe.
+
+        :raises TimeoutError: if timeout is given and no value is acknowledged in the mean time.
+        """
+        if not self._sem.acquire(timeout=timeout):
+            raise TimeoutError('Too many values un-acknowledged.')
+
+        return next(self._it)
+
+    def processed(self):
+        """Acknowledges one value allowing another one to be yielded.
+
+        This method is thread-safe.
+        """
+        self._sem.release()
---
+++ test_bounded_iterator.py
@@ -0,0 +1,44 @@
+import time
+import unittest
+from itertools import count
+
+from bounded_iterator import BoundedIterator
+
+
+def _identity(item):
+    return item
+
+
+def _sleep(secs):
+    time.sleep(secs)
+    return secs
+
+
+class BoundedIteratorTest(unittest.TestCase):
+
+    def test_when_the_iterable_is_empty_then_it_produces_no_results(self):
+        subject = BoundedIterator(10, it=())
+        res = list(subject)
+
+        self.assertEqual([], res)
+
+    def test_when_the_max_number_of_items_were_yielded_then_it_will_not_yield_more(self):
+        subject = BoundedIterator(2, count())
+
+        chunk0 = next(subject), next(subject)
+
+        self.assertEqual((0, 1), chunk0)
+
+        with self.assertRaises(TimeoutError):
+            subject.next(timeout=0.01)
+
+    def test_when_a_value_is_acknowledged_then_it_will_yield_one_more(self):
+        subject = BoundedIterator(2, count())
+
+        chunk0 = next(subject), next(subject)
+        self.assertEqual((0, 1), chunk0)
+        with self.assertRaises(TimeoutError):
+            subject.next(timeout=0.01)
+
+        subject.processed()
+        self.assertEqual(2, next(subject))
