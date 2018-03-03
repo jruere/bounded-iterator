9620ccdcde6d442c1e622b23c4ae067753876fc4
Javier Ruere <javier@ruere.com.ar>
2018-03-03 20:46:26 -0300

    Fixes classifier for PyPy.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: Pypy',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     keywords="concurrency parallelism iterator iterable multiprocessing imap",
