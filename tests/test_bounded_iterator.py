38aee1b7edf7ed286a3e858dfca9f6a86cc95141
Javier Ruere <javier@ruere.com.ar>
2026-09-05 19:56:29 +0200

    Remove dead code.

--- tests/test_bounded_iterator.py
+++ tests/test_bounded_iterator.py
@@ -1,28 +1,16 @@
-import time
 import unittest
 from itertools import count
-from typing import TypeVar
 
 from bounded_iterator import BoundedIterator
-
-T = TypeVar("T")
-
-
-def _identity(item: T) -> T:
-    return item
-
-
-def _sleep(secs: float) -> float:
-    time.sleep(secs)
-    return secs
 
 
 class BoundedIteratorTest(unittest.TestCase):
     def test_when_the_iterable_is_empty_then_it_produces_no_results(self) -> None:
         subject: BoundedIterator[int] = BoundedIterator(10, it=())
-        res = list(subject)
 
-        self.assertEqual([], res)
+        results = list(subject)
+
+        self.assertEqual([], results)
 
     def test_when_the_max_number_of_items_were_yielded_then_it_will_not_yield_more(self) -> None:
         subject = BoundedIterator(2, count())
